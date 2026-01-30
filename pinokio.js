4007ada334d7f7d2cf9c1089a2286ad599fb19a2
cocktailpeanut <cocktailpeanuts@proton.me>
2026-01-30 02:23:46 -0500

    icon

--- pinokio.js
+++ pinokio.js
@@ -1,6 +1,7 @@
 module.exports = {
   version: "5.0",
   title: "OpenClaw Rabbit Launcher",
+  icon: "icon.png",
   description: "Connect your Rabbit R1 device to OpenClaw (Formerly ClawdBot)",
   menu: async (kernel, info) => {
     return [{
